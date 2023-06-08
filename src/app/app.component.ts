8065073fb708f5b613b772edd5247dded314774f
Bruno Augusto da Silva <bruno.sidf@gmail.com>
2023-06-08 15:51:22 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
+const electron = (<any>window).require('electron').ipc;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ftp-desktop';
+
+  teste = signal('');
+
+  constructor() {
+    effect(this.loadFiles);
+    (<any>window).ipcRenderer.on('load-files', (event: any) => {
+      console.log('TESTE');
+      console.log(event);
+    });
+  }
+
+  loadFiles(): void {
+    console.log('iniciado');
+
+    console.log((<any>window).ipcRenderer.on);
+  }
 }
