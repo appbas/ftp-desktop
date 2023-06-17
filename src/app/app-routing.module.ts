30ea8fcb1e9b821671d8c8f7cdf5e3fe43b1e10f
Bruno Augusto da Silva <bruno.sidf@gmail.com>
2023-06-17 20:20:53 -0300

    login and home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,22 +3,27 @@ import { RouterModule, Routes } from '@angular/router';
 import { TemplateComponent } from './core/layouts/template/template.component';
 
 const routes: Routes = [
-
   {
     path: '',
     component: TemplateComponent,
     children: [
       {
         path: '',
-        loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule)
-      }
-    ]
-  }
+        loadChildren: () =>
+          import('./modules/home/home.module').then((m) => m.HomeModule),
+      },
+    ],
+  },
 
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./modules/login/login.module').then((m) => m.LoginModule),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject, Injector } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  private readonly _router = inject(Router);
 
+  onLogin(): void {
+    this._router.navigate(['']);
+  }
 }
